a49457420d1fca20f2a18f78209af7c822089f79
Sam Lukas <iggyboy456@gmail.com>
2019-09-14 10:50:27 -0500

    added functions for timing and checking answers

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,8 @@ var q2 = false;
 var q3 = false;
 var q4 = false;
 var q5 = false;
+var delay;
+var count = 60;
 
 var checkResponse = function () {
     if ($("#q1b:checked").length > 0) {
@@ -40,8 +42,94 @@ var checkResponse = function () {
         console.log("incorrect");
         q4 = false;
     }
+
+    if ($("#q5d:checked").length > 0) {
+        console.log("correct");
+        q5 = true;
+    }
+    else {
+        console.log("incorrect");
+        q5 = false;
+    }
+
+    printer()
 }
 
-$("#sumbit").on("click", function () {
-    checkResponse();
-});
+var printer = function () {
+    if (q1 === true) {
+        $("#q1").text("Correct!");
+    }
+    else {
+        $("#q1").text("Incorrect! the right answer was 94.");
+    }
+
+    if (q2 === true) {
+        $("#q2").text("Correct!");
+    }
+    else {
+        $("#q2").text("Incorrect! the right answer was Elvarg.");
+    }
+
+    if (q3 === true) {
+        $("#q3").text("Correct!");
+    }
+    else {
+        $("#q3").text("Incorrect! the right answer was 175.");
+    }
+
+    if (q4 === true) {
+        $("#q4").text("Correct!");
+    }
+    else {
+        $("#q4").text("Incorrect! the right answer was 70.");
+    }
+
+    if (q5 === true) {
+        $("#q5").text("Correct!");
+    }
+    else {
+        $("#q5").text("Incorrect! the right answer was Dharok.");
+    }
+
+
+}
+
+function run() {
+    clearInterval(delay);
+    delay = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    if (count > 0) {
+        count--;
+        $("#timer").text(count);
+        if (count === 0) {
+            checkResponse();
+            alert("Time Up!");
+            scorechecker();
+            return;
+        }
+    }
+}
+
+function scorechecker(){
+    var score = 0;
+    if(q1 === true){
+        score++;
+    }
+    if(q2 === true){
+        score++;
+    }
+    if(q3 === true){
+        score++;
+    }
+    if(q4 === true){
+        score++;
+    }
+    if(q5 === true){
+        score++;
+    }
+    $("#score").text("You got " + score + "/5 Questions correct!")
+}
+
+run()
